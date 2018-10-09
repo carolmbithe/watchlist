6542edbbdaade87dc67ffce75a1639d0dd74926a
carolmbithe <carombithe97@gmail.com>
2018-10-09 15:47:29 +0300

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 # from .config import DevConfig
 from flask_bootstrap import Bootstrap
 from config import config_options
 
 
 bootstrap = Bootstrap()
+db=SQLAlchemy()
 
 def create_app(config_name):
 
@@ -15,6 +17,7 @@ def create_app(config_name):
 
     #Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     #Will add the views and forms
 
@@ -22,9 +25,14 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
     #setting config
     from .request import configure_request
     configure_request(app)
+
+
 
     return app
 #Initializing application
